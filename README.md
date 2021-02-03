b388512eee12f5520fa5c0bcd5dd061efbd359f0
yunsang6336.cho <yunsang6336.cho@lgepartner.com>
2021-02-03 10:14:43 +0900

    Step1. Adding a Version Number and Configured Header File

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CMakeTutorial
 
 #2021-01-10 : push to github...
+
+#2021-02-03 : Step1 / Adding a Version Number and Configured Header File
--- tutorial.cxx
+++ tutorial.cxx
@@ -2,10 +2,13 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include "TutorialConfig.h" //step1 / Adding a Version Number and Configured Header File
 
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              << Tutorial_VERSION_MINOR << std::endl;
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
